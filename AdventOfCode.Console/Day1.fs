4bdde15813104627a2d710860dfe19b152827ef5
David Tyler <david.tyler@metaswitch.com>
2020-11-30 12:27:06 +0000

    Test inlining lambda for day 1 2019

--- AdventOfCode.Console/Day1.fs
+++ AdventOfCode.Console/Day1.fs
@@ -4,15 +4,12 @@ open Serilog
 open System.Linq
 open System.Collections.Generic
 
-let massToFuel mass =
-    mass / 3 - 2
-
 let run (lines: IEnumerable<string>) =
     Log.Information $"Executing Day 1"
 
     let result =
         lines.Select int
-            |> Seq.map massToFuel
+            |> Seq.map (fun m -> m / 3 - 2)
             |> Seq.sum
 
     Log.Information $"Results is {result}"
